f3c938447100a23b4fe33837c1f29f159967e7e8
Julian Fernandez <juli1423@hotmail.com>
2024-08-29 16:25:39 -0500

    se agrega un nuevo mensaje

--- src/clases/Principal.java
+++ src/clases/Principal.java
@@ -4,8 +4,11 @@ import gui.VentanaPrincipal;
 
 public class Principal {
 
-	
+	      
+
 	public static void main(String arg[]) {
+		            Console.WriteLine("¡Hola hice este cambio, espero te guste!");
+
 		VentanaPrincipal miVentana=new VentanaPrincipal();
 		miVentana.setVisible(true);
 	}
