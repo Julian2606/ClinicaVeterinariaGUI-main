ede4909b5231fad52819193124b0fdb8a537f435
Julian Fernandez <juli1423@hotmail.com>
2024-08-29 16:29:37 -0500

    borre tu cambio, tenia errores

--- src/clases/Principal.java
+++ src/clases/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 	      
 
 	public static void main(String arg[]) {
-		            Console.WriteLine("¡Hola hice este cambio, espero te guste!");
+		            
 
 		VentanaPrincipal miVentana=new VentanaPrincipal();
 		miVentana.setVisible(true);
