d6e377deb93c07cf3569571a4faa16d5db358745
Julian Fernandez <juli1423@hotmail.com>
2024-08-30 00:16:31 -0500

    agregue mensaje al proyecto

--- src/clases/Principal.java
+++ src/clases/Principal.java
@@ -8,7 +8,7 @@ public class Principal {
 
 	public static void main(String arg[]) {
 		            
-
+	Console.WriteLine("¡Hola,agregue algunas cosas!");
 		VentanaPrincipal miVentana=new VentanaPrincipal();
 		miVentana.setVisible(true);
 	}
